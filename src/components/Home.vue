124dbfeb9b87730615cef1dc197b4827e0902bcd
Barry Cronin <barry@handheld.ie>
2019-12-10 09:39:20 +0000

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hero">
+    <h1 class="vue-title">Agree OR Disagree</h1>
+
+    <table align="center">
+      <tr>
+        <td>
+          <p class="lead"><b><i>What sort of PERSON are you?</i></b>
+          <p>Do you find yourself Agreeing pretty much all the time, are you happy in a pack, safety in numbers</p>
+          <p>Or are you someone who has their own point of view, a Lone Wolf, not afraid to stand out from the crowd. </p>
+          <p>Well lets find out...</p>
+        </td>
+        <td>
+          <p>Play this quiz... do you AGREE or DISAGREE with the statements, some fun, some serious, some controversal!!</p>
+          <p> But remember there's no Right OR Wrong answer... (Unless i tell you you're wrong) </p>
+          <p> Its entirely your opinion.. and everyone's opinion is right for them.</p>
+        </td>
+      </tr>
+    </table>
+
+    <p></p>
+
+  </div>
+</template>
+
+<style>
+  .hero {
+    height: 100vh;
+    margin-top: 30px;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+  .hero .lead {
+    font-weight: 200;
+    font-size: 2.5rem;
+  }
+  #app1 {
+    width: 60%;
+    margin: 0 auto;
+  }
+  .vue-title {
+    font-size: 70pt;
+    margin-bottom: 10px;
+  }
+</style>
